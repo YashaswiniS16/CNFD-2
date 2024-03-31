55f5e7a501706db6b091010fb305da51aea97423
YashaswiniS16 <126035768+YashaswiniS16@users.noreply.github.com>
2024-03-31 18:49:11 +0530

    Create Ticket.java

---
+++ Lab1/Ticket.java
@@ -0,0 +1,38 @@
+package com.example.Lab1;
+
+public class Ticket {
+	int ticno,price,seatno;
+	String ticktype;
+	public int getTicno() {
+		return ticno;
+	}
+	public void setTicno(int ticno) {
+		this.ticno = ticno;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getSeatno() {
+		return seatno;
+	}
+	public void setSeatno(int seatno) {
+		this.seatno = seatno;
+	}
+	public String getTicktype() {
+		return ticktype;
+	}
+	public void setTicktype(String ticktype) {
+		this.ticktype = ticktype;
+	}
+	@Override
+	public String toString() {
+		return "Ticket [ticno=" + ticno + ", price=" + price + ", seatno=" + seatno + ", ticktype=" + ticktype
+				+ ", getTicno()=" + getTicno() + ", getPrice()=" + getPrice() + ", getSeatno()=" + getSeatno()
+				+ ", getTicktype()=" + getTicktype() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+
+}
